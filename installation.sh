7cadaa9136834ae3de9cc4e9509b7d0a3c02112d
guhaoping <guhaoping@bytedance.com>
2021-12-20 17:59:29 +0800

    "update"

--- installation.sh
+++ installation.sh
@@ -2,30 +2,13 @@ cp glines.sh ../
 cp kill.sh ../
 
 current_dir=`pwd`
-echo $'alias hls=\'doas hdfs dfs -ls\'' >> ~/.zshrc
-echo $'alias htxt=\'doas hdfs dfs -D io.compression.codec.snappy.native=true -text\'' >> ~/.zshrc
-echo $'alias hdu=\'doas hdfs dfs -du\'' >> ~/.zshrc
-echo $'alias hdu=\'doas hdfs dfs -du\'' >> ~/.zshrc
-echo $'alias hget=\'doas hdfs dfs -get\'' >> ~/.zshrc
-echo $'alias gcheck=\'git checkout -b guhaoping\'' >> ~/.zshrc
-echo $'alias gst=\'git stash\'' >> ~/.zshrc
-echo $'alias gstp=\'git stash pop\'' >> ~/.zshrc
-echo $'alias gfetch=\'git fetch origon master\'' >> ~/.zshrc
-echo $'alias grebase=\'git rebase origin/master\'' >> ~/.zshrc
-echo $'alias gc=\'git commit\'' >> ~/.zshrc
-echo $'alias gp=\'git push origin guhaoping\'' >> ~/.zshrc
-echo $'alias kk=\'kinit -kt /data02/home/guhaoping/keytab/guhaoping.keytab guhaoping\'' >> ~/.zshrc
-echo $'alias bbd=\'blade build . --update-deps --bundle=debug\'' >> ~/.zshrc
-echo $'alias bb=\'blade build . --bundle=debug\'' >> ~/.zshrc
-echo $'alias glines=\'bash ~/glines.sh\'' >> ~/.zshrc
-echo $'alias k=\'bash ~/kill.sh\'' >> ~/.zshrc
-echo $'kinit -kt $current_dir/keytab/guhaoping.keytab guhaoping' >> ~/.zshrc
-echo $'export PYTHONPATH=~/offline_workspace/:$PYTHONPATH' >> ~/.zshrc
+source_root=$(cd `dirname $0`; pwd)
+
+cat $source_root/alias.txt >> ~/.zshrc
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 echo "paste following line to ur ~/.zshrc"
 echo "plugins=(git zsh-syntax-highlighting zsh-autosuggestions)"
-
 
